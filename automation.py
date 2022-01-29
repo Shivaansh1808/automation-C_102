36d2e81b3686df564341a2b559fd1bb63ba61f9b
Shivaansh1808 <72135426+Shivaansh1808@users.noreply.github.com>
2022-01-29 17:49:37 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,34 @@
+import cv2
+import dropbox
+import time
+import random
+
+startTime = time.time()
+print(startTime)
+
+def takeSnapshot():
+    number = random.randint(0,100)
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while result:
+        ret,frame = videoCaptureObject.read()
+        image_name = "img"+str(number)+".png"
+        cv2.imwrite(image_name, frame)
+        startTime = time.time()
+        result = False
+    return image_name
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+
+def uploadFile(image_name):
+    accessToken = "ZXdtn3QwzS8AAAAAAAAAAexqvRHqHMjORLNKVl6aQpPL-u7alSn6fkkJyRomEXow"
+    file_from = image_name
+    file_to = "/securitysystem/"+image_name
+    dbx = dropbox.Dropbox(accessToken)
+    with open(file_from,"rb") as f:
+        dbx.files_upload(f.read(), file_to)
+
+while (True):
+    if time.time()-startTime >= 5:
+        name = takeSnapshot()
+        uploadFile(name)
